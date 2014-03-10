00a17c768de3c80fef65c2f50809576829221d09
pouloghost <864984007@qq.com>
2014-03-10 14:07:35 +0800

    make sure the float window only show once

--- src/gt/toolbox/MainActivity.java
+++ src/gt/toolbox/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends Activity {
 		startService(watcherIntent);
 		startService(excutorIntent);
 
+		manager.stop();
 		manager.createFloatWindow(this, true);
 
 		listenerLV
